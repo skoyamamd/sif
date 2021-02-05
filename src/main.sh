501167c09117b74c5c7c8578a694c6467af4376b
skoyamamd <skoyama@broadinstitute.org>
2021-02-05 03:01:28 +0000

    modify

--- src/main.sh
+++ src/main.sh
@@ -1,21 +1,11 @@
-git clone https://github.com/broadinstitute/gtex-pipeline.git
-
-cd ./gtex-pipeline/rnaseq
-
-git checkout master
-spython recipe Dockerfile > ../../../recipe/Singularity.rnaseq.v9
-
-git checkout -b b_gtex_v8 gtex_v8
-spython recipe Dockerfile > ../../../recipe/Singularity.rnaseq.v8
-
 wget -O ../dockerfile/bamsurgeon.dockerfile \
   https://raw.githubusercontent.com/adamewing/bamsurgeon/master/Dockerfile
 
 spython recipe ../dockerfile/bamsurgeon.dockerfile > ../recipe/Singularity.bamsurgeon
 
-wget -O ../dockerfile/neusomatic.edockerfile \
+wget -O ../dockerfile/neusomatic.dockerfile \
   https://raw.githubusercontent.com/bioinform/neusomatic/master/docker/Dockerfile
 
-spython recipe ../dockerfile/neusomatic.edockerfile > ../recipe/Singularity.neusomatic
+spython recipe ../dockerfile/neusomatic.dockerfile > ../recipe/Singularity.neusomatic
 
 
