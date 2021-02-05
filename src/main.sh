4f1f732116a1ceb4d1bdf5a908d41657da360f9b
skoyamamd <skoyama@broadinstitute.org>
2021-02-05 03:04:59 +0000

    add short name

--- src/main.sh
+++ src/main.sh
@@ -1,11 +1,11 @@
 wget -O ../dockerfile/bamsurgeon.dockerfile \
   https://raw.githubusercontent.com/adamewing/bamsurgeon/master/Dockerfile
 
-spython recipe ../dockerfile/bamsurgeon.dockerfile > ../recipe/Singularity.bamsurgeon
+spython recipe ../dockerfile/bamsurgeon.dockerfile > ../recipe/Singularity.bam
 
 wget -O ../dockerfile/neusomatic.dockerfile \
   https://raw.githubusercontent.com/bioinform/neusomatic/master/docker/Dockerfile
 
-spython recipe ../dockerfile/neusomatic.dockerfile > ../recipe/Singularity.neusomatic
+spython recipe ../dockerfile/neusomatic.dockerfile > ../recipe/Singularity.neu
 
 
